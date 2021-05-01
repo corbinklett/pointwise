0f1f8890d972c8e32b3f2632f50d3b1893cba50f
Corbin Klett <corbinklett@Corbins-MBP.attlocal.net>
2021-04-30 22:30:13 -0400

    vehicle files

---
+++ vehicle/read_gps.py
@@ -0,0 +1,27 @@
+import serial
+
+# First, receive data from the pointwise transmitter
+
+
+
+with serial.Serial('/dev/tty.usbmodem14301', baudrate=4800, timeout=1) as ser:
+    # read 10 lines from the serial output
+    for i in range(50):
+        #line = ser.readline().decode('ascii', errors='replace')
+        #print(line.strip())
+        line = ser.readline().decode('ascii',errors='replace')
+        sen = line[1:6]
+        if sen == 'GPGLL': # or sen == 'GPGGA' or sen == 'GPRMC':
+            chunks = line.split(',')
+            lat = chunks[1]
+            lon = chunks[3]
+
+
+        
+
+
+# Actual position:
+# 33.76580151306169, -84.35375312094449
+
+# Reference:
+# https://www.rfwireless-world.com/Terminology/GPS-sentences-or-NMEA-sentences.html
